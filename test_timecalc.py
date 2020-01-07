bdd33136abeebef0105675ac35be417ebf2030c9
edoego <edoego87@gmail.com>
2020-01-07 16:14:55 +0400

    updated

--- test_timecalc.py
+++ test_timecalc.py
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 import unittest
-import time
+import timecalc
 
 class TestTime(unittest.TestCase):
 
     def test_counter(self):
-        result = time.counter(2, [0,3], [1,5])
+        result = timecalc.counter(2, [0,3], [1,5])
         self.assertEqual(result, 4)
 
 
-
+if __name__ == '__main__':
+    unittest.main()
 
 
 #f = open("unittestresult.txt", "w")
