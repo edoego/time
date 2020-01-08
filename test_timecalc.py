0d143369c857ae22d04c9b4e5fd074b7c21771e6
edoego <edoego87@gmail.com>
2020-01-09 00:49:23 +0400

    test

--- test_timecalc.py
+++ test_timecalc.py
@@ -6,8 +6,9 @@ import timecalc
 class TestTime(unittest.TestCase):
 
     def test_counter(self):
-        result = timecalc.counter(2, [0,3], [1,5])
-        self.assertEqual(result, 4)
+        result = timecalc.counter(2, [0, 3], [1, 5])
+        expected = 4
+        self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
