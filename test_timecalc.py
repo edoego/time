f4cd2d47f324df6ad96837f23ab24ebdd639e825
edoego <58914459+edoego@users.noreply.github.com>
2020-01-22 13:11:39 +0400

    Update test_timecalc.py

--- test_timecalc.py
+++ test_timecalc.py
@@ -12,7 +12,7 @@ class TestTime(unittest.TestCase):
         
     def test_chronos2(self):
         result = timecalc.chronos(3, [-3, 3, 5], [0, 5, -1])
-        expected = 13
+        expected = 16
         self.assertEqual(expected, result)
         
 import sys 
